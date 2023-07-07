c1328952a198269b44cb9460bcb704e47e81c0d2
Damla Oksel <damlaoksel@hotmail.com>
2023-07-07 21:50:11 +0300

    Karakter Kodu yazıldı, powerup kodu yazıldı

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float jumpForce = 5f;
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private bool _isGrounded;
+    private bool _started;
+    private bool _jumping;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _started = false;
+        _isGrounded = true;
+    }
+    private void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            if (_started && _isGrounded)
+            {
+                _animator.SetTrigger("jump");
+                _isGrounded = false;
+                _jumping = true;
+            }
+            else
+            {
+                _animator.SetBool("GameStarted", true);
+                _started = true;
+            }
+        }
+
+        _animator.SetBool("grounded", _isGrounded);
+
+    }
+    private void FixedUpdate()
+    {
+        if (_started)
+        {
+            _rigidbody.velocity = new Vector2(speed*2f, _rigidbody.velocity.y);
+        }
+        if (_jumping)
+        {
+            _rigidbody.AddForce(new Vector2(0f, jumpForce*2f));
+            _jumping = false;
+        }
+    }
+    private void NormalJumpForce()
+    {
+        speed = 10f;
+        jumpForce = 5f;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("powerupjump"))
+        {
+            speed = 20f;
+            _rigidbody.AddForce(transform.up * 10f, ForceMode2D.Impulse);
+            Invoke("NormalJumpForce", 3f);
+        }
+    }
+}
