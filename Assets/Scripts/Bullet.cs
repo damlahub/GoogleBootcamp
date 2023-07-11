cc15cb4a7285e885918eacb256b6cfbdc803ba22
Damla Oksel <damlaoksel@hotmail.com>
2023-07-11 17:51:55 +0300

    turret düzeltildi

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float life = 3;
-
-    void Awake()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject, life);
-    }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(collision.gameObject);
-        Destroy(gameObject);
+        if (collision.gameObject.CompareTag("robotlimit"))
+            Destroy(gameObject);
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,10 +9,17 @@ public class Enemy : MonoBehaviour
     public float bulletSpeed = 10;
 
     
-    void Update()
+    void Start()
     {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.right * bulletSpeed;
-        
+        ShootFunctionRepeat();
+    }
+    void ShootFunctionRepeat()
+    {
+        InvokeRepeating("Shoot", 0.0f, 2.0f);
+    }
+    private void Shoot()
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = -bulletSpawnPoint.right * bulletSpeed;
     }
 }
